7258dcf69f0fa8bc2316dd1c777439412edca9c5
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-03-09 11:50:39 +0100

    rajout soustration

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 console.log("hello world");
 var a= 2
 var b= 3
+
 function addition(a, b){
-	return a + b
+	
 
 }
 console.log(a + b);
-	
 
+function substracion(a, b){
+
+}	
+
+console.log(a - b);
